71b31ea83043e6dd4735b4966d77674e9e30ee6b
Justin Nietzer <justinnietzer@gmail.com>
2021-03-18 21:43:29 -0400

    Feature: Setup calculator class and test

---
+++ src/Calculator.py
@@ -0,0 +1,52 @@
+class Calculator:
+    result = 0
+
+    def __init__(self):
+        pass
+
+# def addition(a,b):
+#     return int(a) + int(b)
+#
+# def subtraction(a,b):
+#     return int(b) - int(a)
+
+# def multiplication(a,b):
+#     return float(a) * float(b)
+#
+# def division(a,b):
+#     if float(a) is not float(0):
+#         return round(float(b) / float(a),9)
+#     else:
+#         return 'Divisor can not be zero.'
+#
+
+# def square(a):
+#     return float(a)**2
+#
+# def square_root(a):
+#     return round(float(a) ** (1 / 2.0),7)
+
+
+    # def add(self,a,b):
+    #     self.result = addition(a,b)
+    #     return self.result
+    #
+    # def subtract(self,a,b):
+    #     self.result = subtraction(a,b)
+    #     return self.result
+    #
+    # def multiply(self,a,b):
+    #     self.result = multiplication(a,b)
+    #     return self.result
+    #
+    # def divide(self,a,b):
+    #     self.result = division(a,b)
+    #     return self.result
+    #
+    # def sqr(self,a):
+    #     self.result = square(a)
+    #     return self.result
+    #
+    # def sqrt(self,a):
+    #     self.result = square_root(a)
+    #     return self.result
---
+++ src/CalculatorTests.py
@@ -0,0 +1,64 @@
+import unittest
+from Calculator import  Calculator
+
+class MyTestCase(unittest.TestCase):
+
+
+ def test_instantiate_calculator(self):
+     calculator = Calculator()
+     self.assertIsInstance(calculator, Calculator)
+
+    # def setUp(self) -> None:
+    #     self.calculator = Calculator()
+    #
+
+    # def test_addition_method_calculator(self):
+    #    test_data = CsvReader('/src/Unit Test Addition.csv').data
+    #    #pprint(test_data)
+    #    for row in test_data:
+    #        self.assertEqual(self.calculator.add(row['Value 1'],row['Value 2']),int(row['Result']))
+    #        self.assertEqual(self.calculator.result,int(row['Result']))
+    #    test_data.clear()
+    #
+    # def test_subtraction_method_calculator(self):
+    #     test_data = CsvReader('/src/Unit Test Subtraction.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.subtract(row['Value 1'],row['Value 2']),int(row['Result']))
+    #         self.assertEqual(self.calculator.result,int(row['Result']))
+    #     test_data.clear()
+    #
+    # def test_multiplication_method_calculator(self):
+    #     test_data = CsvReader('/src/Unit Test Multiplication.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.multiply(row['Value 1'],row['Value 2']),float(row['Result']))
+    #         self.assertEqual(self.calculator.result,float(row['Result']))
+    #     test_data.clear()
+    #
+    # def test_division_method_calculator(self):
+    #     test_data = CsvReader('/src/Unit Test Division.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.divide(row['Value 1'],row['Value 2']),float(row['Result']))
+    #         self.assertEqual(self.calculator.result,float(row['Result']))
+    #     test_data.clear()
+    #
+    # def test_square_method_calculator(self):
+    #     test_data = CsvReader('/src/Unit Test Square.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.sqr(row['Value 1']),float(row['Result']))
+    #         self.assertEqual(self.calculator.result,float(row['Result']))
+    #     test_data.clear()
+    #
+    # def test_square_root_method_calculator(self):
+    #     test_data = CsvReader('/src/Unit Test Square Root.csv').data
+    #     #pprint(test_data)
+    #     for row in test_data:
+    #         self.assertEqual(self.calculator.sqrt(row['Value 1']),round(float(row['Result']),7))
+    #         self.assertEqual(self.calculator.result,round(float(row['Result']),7))
+    #     test_data.clear()
+
+if __name__ == '__main__':
+    unittest.main()
